9c4b65480cb9a37cd45ebfd4f7f266a8267c84db
Andrea Censi <andrea@cds.caltech.edu>
2013-05-03 01:00:00 -0700

    diffeo demo

--- boot_servo_demo/src/servo_demo2.py
+++ boot_servo_demo/src/servo_demo2.py
@@ -2,32 +2,37 @@
 import roslib; roslib.load_manifest('boot_servo_demo')  # @IgnorePep8
 
 from boot_servo_demo.msg import Raw
+from bootstrapping_olympics.configuration.master import get_boot_config
 from bootstrapping_olympics.programs.manager.meat.data_central import (
     DataCentral)
 from bootstrapping_olympics.programs.manager.meat.load_agent_state import (
     load_agent_state)
 from bootstrapping_olympics.utils.safe_pickle import (safe_pickle_load,
     safe_pickle_dump)
+from conf_tools.master import GlobalConfig
 from contracts import contract
 from ros_node_utils import ROSNode
 from rospy.numpy_msg import numpy_msg
-from rosstream2boot.config import get_rs2b_config
+from rosstream2boot.programs.rs2b_convert2 import bd_sequence_from_robot
 from std_msgs.msg import String
 from std_srvs.srv import Empty, EmptyResponse
 import numpy as np
 import os
-import rospy
+import rospy 
+from ros_node_utils.nodes.ros_logger import RospyLogger
+from bootstrapping_olympics.interfaces.agent import AgentInterface
+import contracts
 import warnings
-from bootstrapping_olympics.interfaces.observations import ObsKeeper
-from rosstream2boot.programs.rs2b_convert2 import iterate_robot_observations
+from conf_tools.utils.expansion import expand_environment
 import time
- 
 
 nraw = numpy_msg(Raw)
 
 
 STATE_WAIT = 'wait'
 STATE_SERVOING = 'servoing'
+
+
 
 class ServoDemo2(ROSNode):
     """ 
@@ -40,10 +45,8 @@ class ServoDemo2(ROSNode):
             
             id_robot_learned: 
         
-        Input:
-        
-        Output:
-        
+            config_dir: list of directories
+                    
     """
     state_filename = 'y_goal.pickle'
     
@@ -52,39 +55,52 @@ class ServoDemo2(ROSNode):
         
 
     def main(self):
+        AgentInterface.logger = RospyLogger('agents') 
+        
+        # find_print_statements()
         rospy.init_node('servo_demo', disable_signals=True)
         
         self.info('Started.')
+        contracts.disable_all()
 
         boot_root = rospy.get_param('~boot_root')
-        rs2b_config_dir = rospy.get_param('~config_dir')
-        id_robot_learned = rospy.get_param('~id_robot_learn')
-        id_agent = rospy.get_param('~id_agent')
-        id_robot = rospy.get_param('~id_robot')
-        self.error_threshold = float(rospy.get_param('~error_threshold'))
-        self.K = np.array(eval(rospy.get_param('~K')))
+        boot_root = expand_environment(boot_root)
         
-        rs2b_config = get_rs2b_config()
-        rs2b_config.load(rs2b_config_dir)
+        config_dir = rospy.get_param('~config_dir')
+        id_robot_learned = rospy.get_param('~id_robot_learn')
+        
+        self.info('loading %r' % config_dir)
+        GlobalConfig.global_load_dir(config_dir)
+        
+        id_agent = rospy.get_param('~id_agent')
+        self.id_robot = rospy.get_param('~id_robot')
+        self.sleep = rospy.get_param('~sleep', 0.005)
+        self.info('sleep: %s' % self.sleep)
+        self.error_threshold = float(rospy.get_param('~error_threshold'))
+        
+        raise_if_no_state = rospy.get_param('~raise_if_no_state', True)
+        
         data_central = DataCentral(boot_root)
-        bo_config = data_central.get_bo_config()
-        bo_config.load(rs2b_config_dir)
         
         ag_st = load_agent_state(data_central, id_agent, id_robot_learned,
-                                 reset_state=False, raise_if_no_state=True)
+                                 reset_state=False,
+                                 raise_if_no_state=raise_if_no_state)
         self.agent, state = ag_st
         
         self.info('Loaded state: %s' % state)
         
         self.servo_agent = self.agent.get_servo()
-
-        self.robot = bo_config.robots.instance(id_robot)
+        
+        bo_config = get_boot_config()
+        self.robot = bo_config.robots.instance(self.id_robot)
+        self.boot_spec = self.robot.get_spec()
             
         self.publish_info_init()     
         
         self.y = None
         self.y_goal = None
         self.started_now = False
+        self.stopped_now = False
         self.e0 = 1
         self.e = 1         
         self.last_boot_data = None
@@ -95,31 +111,31 @@ class ServoDemo2(ROSNode):
         rospy.Service('start_servo', Empty, self.srv_start_servo)
         rospy.Service('stop_servo', Empty, self.srv_stop_servo)
                 
-        self.info('Finished initialization')
-        
-        self.obs_keeper = ObsKeeper(boot_spec=self.robot.get_spec(),
-                                id_robot=id_robot)
-        
+        self.info('Finished initialization') 
         self.count = 0
         self.go()
         
     def go(self):
+        id_episode = 'XXX'  # XXX
+        id_environment = 'xxx'  # XXX
+
+        bd_seq = bd_sequence_from_robot(id_robot=self.id_robot,
+                                     robot=self.robot,
+                                     sleep_wait=self.sleep,
+                                     id_episode=id_episode,
+                                     id_environment=id_environment,
+                                     check_valid_values=False)
         
-        for obs in iterate_robot_observations(self.robot, sleep=0.005):
-            self.count += 1
-            timestamp = obs.timestamp
-            print('delay: %10.5f' % (time.time() - timestamp))
-            observations = obs.observations
-            commands = obs.commands
-            commands_source = obs.commands_source
-            warnings.warn('fill these up')
-            id_episode = 'XXX'  # XXX
-            id_world = 'xxx'  # XXX
-            boot_data = self.obs_keeper.push(timestamp, observations,
-                                             commands, commands_source,
-                                             id_episode, id_world)
-            self.process(boot_data)
-                    
+        try:
+            for bd in bd_seq: 
+                self.process(bd)
+        except KeyboardInterrupt:
+            self.info('Keyboard interrupt-sending stop')
+            self._send_rest()
+#             for bd in bd_seq:
+#                 u = bd['commands']
+#                 print('cmd is %s' % str(u))
+         
 
     def srv_set_goal(self, req):  # @UnusedVariable
         self.info('called "set_goal"')
@@ -138,13 +154,23 @@ class ServoDemo2(ROSNode):
     def srv_stop_servo(self, req):  # @UnusedVariable
         self.info('called "stop_servo"')
         self.state = STATE_WAIT       
+        self.stopped_now = True
         return EmptyResponse()
  
+    def check_delay(self, bd):
+        ts = bd['timestamp'].item()
+        then = rospy.Time.from_seconds(ts)
+        now = rospy.Time.now()
+        delay = (now - then).to_sec()
+        if delay > 0.1:
+            print('delay: %4.2fsec' % delay)
+        
     def process(self, boot_data):
+        self.check_delay(boot_data)
+        
         self.last_boot_data = boot_data.copy()
         
         self.u = self.get_servo_commands(boot_data)
-        
         self.publish_info()
         
         pc = lambda x: ' '.join(['%+6.3f' % a for a in x]) 
@@ -162,13 +188,18 @@ class ServoDemo2(ROSNode):
         elif self.state == STATE_SERVOING:
             if verbose:
                 self.info('send %s' % msg)
-                
-            if True:
-                warnings.warn('Cehcking if we are getting overloaded')
-                if self.count % 2 == 0:
-                    self.robot.set_commands(self.u, 'servo')
+            self.robot.set_commands(self.u, 'servo')
         
+        if self.stopped_now:
+            self._send_rest()
+            self.stopped_now = False
+
+    def _send_rest(self):
+        print('sending rest command')
+        rest = self.robot.get_spec().get_commands().get_default_value()
+        self.robot.set_commands(rest, 'rest')
         
+
     def get_servo_commands(self, boot_data):
         self.y = boot_data['observations']
         
@@ -196,17 +227,14 @@ class ServoDemo2(ROSNode):
 
         self.servo_agent.process_observations(boot_data)
     
-        u = self.servo_agent.choose_commands() 
-
-#         warnings.warn('Some extra spice')
-#         u[0] = u[0] * 0.2
-# #         u[1] = 0
-#         self.info('scaled: %s' % str(u))
+        u = self.servo_agent.choose_commands()         
+            
+        self.boot_spec.get_commands().check_valid_value(u)
 
         if self.state == STATE_SERVOING:    
             self.e = self.get_distance_to_goal(self.y)
             if self.e < self.error_threshold:
-                print('stopping here')
+                self.info('stopping here')
                 return u * 0 
     
         return u
